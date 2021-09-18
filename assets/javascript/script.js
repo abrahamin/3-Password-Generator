68f6c75d8f2a2454937dcd44dba692682d148187
Abraham Hong <hhmfjg@gmail.com>
2021-09-17 22:47:22 -0400

    implemented better validation and randomization

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,12 +12,14 @@ var characterLimit = window.prompt("Please indicate desired length of generated 
 
 if (characterLimit < 8 || characterLimit > 128 || characterLimit.includes(".") || isNaN(characterLimit)) {
   window.alert("Your response does not meet the requirements.");
-  return;
+  return ("");
 };
 
 //    b. Lowercase, uppercase, numeric, special characters
 
 var chosenOptions = [];
+
+var counter = 0;
 
 var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -35,71 +37,88 @@ var numericChar = window.confirm("Would you like to include numeric values?");
 
 var specialChar = window.confirm("Would you like to include special characters?");
 
+var finalArray = [];
+
+//  2. Validate the input
+
 if (lowerCase) {
-  chosenOptions = chosenOptions.concat(lowerCaseOptions)
+  chosenOptions = chosenOptions.concat(lowerCaseOptions);
+  var randomLower = lowerCaseOptions[Math.floor(Math.random()*lowerCaseOptions.length)];
+  finalArray.unshift(randomLower);
+  counter++;
 };
 
 if (upperCase) {
-  chosenOptions = chosenOptions.concat(upperCaseOptions)
+  chosenOptions = chosenOptions.concat(upperCaseOptions);
+  var randomUpper = upperCaseOptions[Math.floor(Math.random()*upperCaseOptions.length)];
+  finalArray.unshift(randomUpper);
+  counter++;
 };
 
 if (numericChar) {
-  chosenOptions = chosenOptions.concat(numericOptions)
+  chosenOptions = chosenOptions.concat(numericOptions);
+  var randomNumeric = numericOptions[Math.floor(Math.random()*numericOptions.length)];
+  finalArray.unshift(randomNumeric);
+  counter++;
 };
 
 if (specialChar) {
-  chosenOptions = chosenOptions.concat(specialOptions)
+  chosenOptions = chosenOptions.concat(specialOptions);
+  var randomSpecial = specialOptions[Math.floor(Math.random()*specialOptions.length)];
+  finalArray.unshift(randomSpecial);
+  counter++;
 };
 
 if (chosenOptions.length === 0) {
   window.alert("Your response does not meet the requirements.");
-  return;
+  return ("");
 };
 
-//  2. Generate password that matches the selected criteria
+console.log(chosenOptions);
+console.log(finalArray);
+console.log(counter);
 
-var final = "";
+//  3. Generate password that matches the selected criteria
+
+var finalString = finalArray.join("");
+
+console.log(finalString);
 
 function generator() {
-  for(var i=0; i <= characterLimit; i++){
-  final += chosenOptions[Math.floor(Math.random() * chosenOptions.length)]
+  for(var i=0; i < characterLimit - counter; i++){
+  finalString += chosenOptions[Math.floor(Math.random() * chosenOptions.length)]
 }
 };
 
 generator();
 
-console.log(final);
+console.log(finalString);
 
-//  3. Validate the input
+var randomArray = finalString.split('');
 
-var finalArray = final.split("")
+console.log(randomArray);
 
-function validCheck(x,y) {
-  for(var i=0; i < x.length; i++) {
-    for(var j=0; j < y.length; j++) {
-      if (x[i] === y[j]) {
-        return true + x[i];
-      }
-    }
-  } return false;
-}
-
-if (lowerCase && validCheck(finalArray, lowerCaseOptions) === false) {
-  final = "";
-  generator();
-} else if (upperCase && validCheck(finalArray, upperCaseOptions) === false) {
-  final = "";
-  generator();
-} else if (numericChar && validCheck(finalArray, numericOptions) === false) {
-  final = "";
-  generator();
-} else if (specialChar && validCheck(finalArray, specialOptions) === false) {
-  final = "";
-  generator();
+function randomize(randomArray) {
+  var currentIndex = randomArray.length, randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random()* currentIndex);
+    currentIndex--;
+    [randomArray[currentIndex], randomArray[randomIndex]] = [
+      randomArray[randomIndex], randomArray[currentIndex]];
+  }
+  return randomArray
 };
 
+randomize(randomArray);
+
+console.log(randomArray);
+
+var finalPassword = randomArray.join("");
+
+console.log(finalPassword);
+
 //  4. Display password to the page
-  return final;
+  return finalPassword;
 }
 
 // Write password to the #password input
